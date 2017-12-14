04b49f747d21fe44ddbbbcd0cd77f24baa545904
Jim Hester <james.f.hester@gmail.com>
2017-12-14 10:03:51 -0500

    Fix building via R CMD check

--- R/rename.R
+++ R/rename.R
@@ -1,7 +1,7 @@
 #' Rename a file
 #'
 #' @param path file path
-#' @param path new file path
+#' @param new_path new file path
 #' @export
 file_rename <- function(path, new_path) {
   file_rename_(normalizePath(enc2utf8(path)), normalizePath(enc2utf8(new_path), mustWork = FALSE))
