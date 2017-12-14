f1310fa026d14eee9ca7ecbc28dafb861aa12fc1
Jim Hester <james.f.hester@gmail.com>
2017-12-14 11:49:01 -0500

    Add a tilde expansion function

---
+++ R/expand.R
@@ -0,0 +1,9 @@
+#' Perform tilde expansion of a pathname
+#'
+#' equivalent to [base::path.expand]
+#' @export
+file_expand <- function(path) {
+  enc2utf8(path.expand(enc2utf8(path)))
+}
+
+# TODO: so far it looks like libuv does not provide a cross platform version of this https://github.com/libuv/libuv/issues/11
