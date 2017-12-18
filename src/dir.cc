91a14b002cc5d85024c217572c83bb8e8f2bccfb
Jim Hester <james.f.hester@gmail.com>
2017-12-18 10:11:25 -0500

    Rename variable

--- src/dir.cc
+++ src/dir.cc
@@ -10,15 +10,17 @@
 #include "utils.h"
 #include "uv.h"
 
+using namespace Rcpp;
+
 // [[Rcpp::export]]
 void mkdir_(CharacterVector path, std::string mode_str) {
   void* out = setmode(mode_str.c_str());
   mode_t mode = getmode(out, 0);
 
   for (size_t i = 0; i < Rf_xlength(path); ++i) {
-    uv_fs_t file_req;
+    uv_fs_t req;
     const char* p = CHAR(STRING_ELT(path, i));
-    int fd = uv_fs_mkdir(uv_default_loop(), &file_req, p, mode, NULL);
+    int fd = uv_fs_mkdir(uv_default_loop(), &req, p, mode, NULL);
     stop_for_error("Failed to make directory", p, fd);
     uv_fs_req_cleanup(&file_req);
   }
