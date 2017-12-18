f3a61c779bb510478de197af5f83937bee43b449
Jim Hester <james.f.hester@gmail.com>
2017-12-18 16:28:10 -0500

    Add tests for symbolic links with cycles

--- tests/testthat/test-dir_list.R
+++ tests/testthat/test-dir_list.R
@@ -1,9 +1,10 @@
 context("test-dir_list.R")
 
-describe("dir_list",
-  it("Does not include '.' in results", {
+describe("dir_list", {
+  it("Does not include '.' or double '/' in results", {
     with_dir_tree(list("foo" = "test"), {
       expect_equal(dir_list(), "foo")
+      expect_equal(dir_list("."), "foo")
       expect_equal(dir_list("./"), "./foo")
     })
 
@@ -15,4 +16,12 @@ describe("dir_list",
       expect_equal(dir_list("foo/"), "foo/bar")
     })
   })
-)
+
+  it("Does not follow symbolic links", {
+    with_dir_tree(list("foo/bar/baz" = "test"), {
+      link_create("foo", "foo/bar/qux")
+      expect_equal(dir_list(), c("foo", "foo/bar", "foo/bar/baz", "foo/bar/qux"))
+      expect_equal(dir_list(type = "symlink"), "foo/bar/qux")
+    })
+  })
+})
