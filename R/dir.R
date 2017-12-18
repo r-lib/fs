0b363f94cc091595942c4ad05b74e5006050d96d
Jim Hester <james.f.hester@gmail.com>
2017-12-18 17:34:49 -0500

    Move dir_list into it's own file

--- R/dir.R
+++ R/dir.R
@@ -33,40 +33,6 @@ dir_create <- function(path, mode = "u+rwx,go+rx", recursive = TRUE) {
   invisible(path)
 }
 
-#' List files in a directory
-#'
-#' @param type File type to return, one of "any", "file", "directory",
-#'   "symlink", "FIFO", "socket", "character_device" or "block_device".
-#' @param recursive Should directories be listed recursively?
-#' @param pattern A regular expression pattern used passed to [grep] to filter
-#'   the filenames.
-#' @param ... Additional arguments passed to [grep].
-#' @template fs
-#' @export
-#' @examples
-#' dir_list(system.file())
-dir_list <- function(path = ".", recursive = TRUE, type = "any", pattern = NULL, ...) {
-  directory_entry_types <- c(
-    "any" = -1L,
-    "file" = 1L,
-    "directory" = 2L,
-    "symlink" = 3L,
-    "FIFO" = 4L,
-    "socket" = 5L,
-    "character_device" = 6L,
-    "block_device" = 7L)
-
-  type <- match.arg(type, names(directory_entry_types))
-
-  path <- path_expand(path)
-
-  files <- scandir_(path, directory_entry_types[type], recursive)
-  if (!is.null(pattern)) {
-    files <- grep(x = files, pattern = pattern, value = TRUE, ...)
-  }
-  files
-}
-
 #' Delete files in a directory
 #'
 #' @inheritParams dir_list
