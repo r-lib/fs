f1a70ef4232ff9621f786532ed2097cfe5f4da58
hadley <h.wickham@gmail.com>
2017-12-18 12:50:44 -0600

    Qualify usage of setNames

--- R/is.R
+++ R/is.R
@@ -16,19 +16,19 @@
 #' is_link(paths)
 is_file <- function(path) {
   res <- file_info(path)
-  setNames(res$type == "file", res$path)
+  stats::setNames(res$type == "file", res$path)
 }
 
 #' @rdname is_file
 #' @export
 is_dir <- function(path) {
   res <- file_info(path)
-  setNames(res$type == "directory", res$path)
+  stats::setNames(res$type == "directory", res$path)
 }
 
 #' @rdname is_file
 #' @export
 is_link <- function(path) {
   res <- file_info(path)
-  setNames(res$type == "symlink", res$path)
+  stats::setNames(res$type == "symlink", res$path)
 }
