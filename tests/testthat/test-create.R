28cbf84d1942f1b6382e581c6dac77d7e6730172
hadley <h.wickham@gmail.com>
2017-12-19 16:45:24 -0600

    Test that link_create matches dir_create and file_create
    Currently failing, cc @jimhester

--- tests/testthat/test-create.R
+++ tests/testthat/test-create.R
@@ -13,3 +13,15 @@ test_that("dir_create works with new and existing files", {
   expect_true(file_exists(x1))
   expect_error(dir_create(x1), NA)
 })
+
+test_that("link_create does not modify existing links", {
+  skip("currently failing")
+  x <- dir_create(tempfile())
+
+  file_create(path(x, "file1"))
+  file_create(path(x, "file2"))
+
+  link_create(path(x, "file1"), path(x, "link"))
+  link_create(path(x, "file2"), path(x, "link"))
+  expect_equal(link_path(path(x, "link"))[[1]], path(x, "file1"))
+})
