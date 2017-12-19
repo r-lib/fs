f03dd69f1fe8d5eb7de0b281c12e38d35bf99415
Jim Hester <james.f.hester@gmail.com>
2017-12-19 17:13:32 -0500

    Add simple dir_copy implementation
    Only works on 1 to 1 copies right now

--- R/dir.R
+++ R/dir.R
@@ -31,3 +31,25 @@ dir_delete <- function(path) {
 dir_exists <- function(path) {
   file_exists(path) && is_dir(path)
 }
+
+#' Copy a directory
+#' @template fs
+#' @param new_path new location to copy to.
+dir_copy <- function(path, new_path) {
+  path <- path_expand(path)
+  new_path <- path_expand(new_path)
+
+  stopifnot(is_dir(path))
+  stopifnot(!dir_exists(new_path))
+
+  dirs <- dir_list(path, type = "directory")
+  dir_create(c(new_path, dirs))
+  files <- setdiff(dir_list(path), dirs)
+
+  # Remove first path from filenames and prepend new path
+  new_files <- path(new_path, sub(".*/", "", files))
+
+  file_copy(files, new_files)
+
+  invisible(new_path)
+}
