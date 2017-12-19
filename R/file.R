ac64556664751008af71edd9a25f9dc24206bb59
Jim Hester <james.f.hester@gmail.com>
2017-12-19 08:46:45 -0500

    Put file_move in it's own directory

--- R/file.R
+++ R/file.R
@@ -1,26 +1,3 @@
-#' Rename a file
-#'
-#' @template fs
-#' @param new_path new file path
-#' @export
-file_move <- function(path, new_path) {
-  path <- path_expand(path)
-  new_path <- path_expand(new_path)
-
-  is_directory <- is_dir(new_path)
-
-  if (length(new_path) == 1 && is_directory[[1]]) {
-    new_path <- rep(new_path, length(path))
-  }
-  stopifnot(length(path) == length(new_path))
-
-  new_path[is_directory] <- path(new_path[is_directory], basename(path))
-
-  move_(path, new_path)
-
-  invisible(new_path)
-}
-
 #' Query file metadata
 #' @template fs
 #' @return A tibble with metadata for each file
---
+++ R/file_move.R
@@ -0,0 +1,22 @@
+#' Move a file
+#'
+#' @template fs
+#' @param new_path new file path
+#' @export
+file_move <- function(path, new_path) {
+  path <- path_expand(path)
+  new_path <- path_expand(new_path)
+
+  is_directory <- is_dir(new_path)
+
+  if (length(new_path) == 1 && is_directory[[1]]) {
+    new_path <- rep(new_path, length(path))
+  }
+  stopifnot(length(path) == length(new_path))
+
+  new_path[is_directory] <- path(new_path[is_directory], basename(path))
+
+  move_(path, new_path)
+
+  invisible(new_path)
+}
