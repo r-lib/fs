289e2dae2677a27389d7745c9f72d66bc75a041d
Jim Hester <james.f.hester@gmail.com>
2018-01-05 09:52:06 -0500

    Examples for id

--- R/id.R
+++ R/id.R
@@ -1,13 +1,19 @@
 #' Lookup Users and Groups on a system
 #'
 #' These functions use the GETPWENT(3) and GETGRENT(3) system calls to query
-#' users and groups respectively. 
+#' users and groups respectively.
 #'
 #' @return They return their results in a `data.frame`. On windows both
 #'   functions return an empty `data.frame` because windows does not have user
 #'   or group ids.
 #' @name id
 #' @export
+#' @examples
+#' # list all groups
+#' group_ids()
+#'
+#' # list all users
+#' user_ids()
 group_ids <- function() {
   res <- groups_()
   res <- unique(res[order(res$group_id), ])
