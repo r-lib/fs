4f18b3d2592d8146309648f7e8a04da17bb7e8d2
Jim Hester <james.f.hester@gmail.com>
2018-01-05 09:49:57 -0500

    Better examples for delete functions

--- R/delete.R
+++ R/delete.R
@@ -9,10 +9,33 @@
 #' @name delete
 #' @examples
 #' \dontshow{fs:::pkgdown_tmp("/tmp/filedd4635bd7c86")}
-#' x <- file_create(file_temp())
-#' file_exists(x)
-#' file_delete(x)
-#' file_exists(x)
+#' # create a directory, with some files and a link to it
+#' dir <- dir_create(file_temp())
+#' files <- file_create(path(dir, letters[1:5]))
+#' link <- link_create(path_norm(dir), "link")
+#'
+#' # All files created
+#' dir_exists(dir)
+#' file_exists(files)
+#' link_exists(link)
+#' file_exists(link_path(link))
+#'
+#' # Delete a file
+#' file_delete(files[1])
+#' file_exists(files[1])
+#'
+#' # Delete the directory (which deletes the files as well)
+#' dir_delete(dir)
+#' file_exists(files)
+#' dir_exists(dir)
+#'
+#' # The link still exists, but what it points to does not.
+#' link_exists(link)
+#' dir_exists(link_path(link))
+#'
+#' # Delete the link
+#' link_delete(link)
+#' link_exists(link)
 file_delete <- function(path) {
   path <- path_expand(path)
   unlink_(path)
