175b057c3dd7712702af7d3849758e22dd393a00
Jim Hester <james.f.hester@gmail.com>
2018-01-05 09:54:12 -0500

    Update link_path documentation

--- R/link.R
+++ R/link.R
@@ -1,11 +1,13 @@
 #' Read the value of a symbolic link
+#'
+#' @return A tidy path to the object the link points to.
 #' @template fs
+#' @export
 #' @examples
 #' file_create("foo")
 #' link_create(path_norm("foo"), "bar")
 #' link_path("bar")
 #' file_delete(c("foo", "bar"))
-#' @export
 link_path <- function(path) {
   path <- path_expand(path)
 
