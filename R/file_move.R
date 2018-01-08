908ee9add23ee0d3fc86572d46f58eb575b3ff32
Jim Hester <james.f.hester@gmail.com>
2018-01-08 12:03:12 -0500

    Expand file_move documentation

--- R/file_move.R
+++ R/file_move.R
@@ -1,11 +1,19 @@
 #' Move or rename files
 #'
+#' Compared to [file.rename] `file_move()` always fails if it is unable to move
+#' a file, rather than signaling a Warning and returning an error code.
 #' @template fs
 #' @param new_path New file path. If `new_path` is existing directory, the file
 #'   will be moved into that directory; otherwise it will be moved/renamed to
 #'   the full path.
 #'
 #'   Should either be the same length as `path`, or a single directory.
+#' @return The new path (invisibly).
+#' @examples
+#' file_create("foo")
+#' file_move("foo", "bar")
+#' file_exists(c("foo", "bar"))
+#' file_delete("bar")
 #' @export
 file_move <- function(path, new_path) {
   path <- path_expand(path)
