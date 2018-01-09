a8ac3ecd6d2fb1b559937d6dc6acd9e9812075cc
Jim Hester <james.f.hester@gmail.com>
2018-01-09 14:17:45 -0500

    Check group_ids() and user_ids() return 0 row dfs

--- tests/testthat/test-id.R
+++ tests/testthat/test-id.R
@@ -1,20 +1,37 @@
 context("test-id.R")
 
-describe("group_ids()", {
-  it("returns the group ids", {
-    skip_on_os("windows")
-    g <- group_ids()
-    expect_is(g, "data.frame")
-    expect_true(nrow(g) > 0)
-    expect_equal(colnames(g),  c("group_id", "group_name"))
+if (is_windows()) {
+  describe("group_ids()", {
+    it("returns the group ids", {
+      g <- group_ids()
+      expect_is(g, "data.frame")
+      expect_true(nrow(g) == 0)
+      expect_equal(colnames(g),  c("group_id", "group_name"))
+    })
   })
-})
-describe("group_ids()", {
-  it("returns the group ids", {
-    skip_on_os("windows")
-    u <- user_ids()
-    expect_is(u, "data.frame")
-    expect_true(nrow(u) > 0)
-    expect_equal(colnames(u),  c("user_id", "user_name"))
+  describe("user_ids()", {
+    it("returns the group ids", {
+      u <- user_ids()
+      expect_is(u, "data.frame")
+      expect_true(nrow(u) == 0)
+      expect_equal(colnames(u),  c("user_id", "user_name"))
+    })
   })
-})
+} else {
+  describe("group_ids()", {
+    it("returns the group ids", {
+      g <- group_ids()
+      expect_is(g, "data.frame")
+      expect_true(nrow(g) > 0)
+      expect_equal(colnames(g),  c("group_id", "group_name"))
+    })
+  })
+  describe("user_ids()", {
+    it("returns the group ids", {
+      u <- user_ids()
+      expect_is(u, "data.frame")
+      expect_true(nrow(u) > 0)
+      expect_equal(colnames(u),  c("user_id", "user_name"))
+    })
+  })
+}
