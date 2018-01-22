8155b954c4f7a7c47982999db913191d2e6e1c34
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-01-21 23:06:15 -0800

    Use uv_fs_realpath() instead of uv_fs_readlink()
    Fixes #43

--- src/link.cc
+++ src/link.cc
@@ -47,7 +47,7 @@ CharacterVector readlink_(CharacterVector path) {
   for (R_xlen_t i = 0; i < Rf_xlength(path); ++i) {
     uv_fs_t req;
     const char* p = CHAR(STRING_ELT(path, i));
-    uv_fs_readlink(uv_default_loop(), &req, p, NULL);
+    uv_fs_realpath(uv_default_loop(), &req, p, NULL);
     SET_STRING_ELT(out, i, Rf_mkCharCE((const char*)req.ptr, CE_UTF8));
     stop_for_error(req, "Failed to read link '%s'", p);
     uv_fs_req_cleanup(&req);
