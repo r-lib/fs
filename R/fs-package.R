36316886ddc0789e35cdd9d5a784a4daf08f3f5a
Jim Hester <james.f.hester@gmail.com>
2018-02-27 13:20:23 -0500

    Add lifecycle badge to the package documentation
    cc @lionel-

--- R/fs-package.R
+++ R/fs-package.R
@@ -1,2 +1,12 @@
+#' @description
+#' \ifelse{html}{
+#' \out{
+#' <a href="https://www.tidyverse.org/lifecycle/#stable">
+#'   <img src="https://img.shields.io/badge/lifecycle-stable-green.svg" alt="Stable lifecycle">
+#' </a>
+#' }
+#' }{
+#' \url{https://www.tidyverse.org/lifecycle/#stable}
+#' }
 #' @keywords internal
 "_PACKAGE"
