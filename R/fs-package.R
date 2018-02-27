0d386610c74caf855423667ec50af2d28b1347d2
Jim Hester <james.f.hester@gmail.com>
2018-02-27 13:42:50 -0500

    Should be maturing
    [ci skip]

--- R/fs-package.R
+++ R/fs-package.R
@@ -1,8 +1,8 @@
 #' @description
 #' \ifelse{html}{
 #' \out{
-#' <a href="https://www.tidyverse.org/lifecycle/#stable">
-#'   <img src="https://img.shields.io/badge/lifecycle-stable-green.svg" alt="Stable lifecycle">
+#' <a href="https://www.tidyverse.org/lifecycle/#maturing">
+#'   <img src="https://img.shields.io/badge/lifecycle-maturing-blue.svg" alt="Stable lifecycle">
 #' </a>
 #' }
 #' }{
