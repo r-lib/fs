eb321996c7ea846a7fa3701e37f26db63e406d3f
Jim Hester <james.f.hester@gmail.com>
2020-06-30 09:19:49 -0400

    Do not link to purrr::map_dfr
    Since purrr is not in Suggests

--- R/list.R
+++ R/list.R
@@ -3,7 +3,7 @@
 #' @description
 #' `dir_ls()` is equivalent to the `ls` command. It returns filenames as a
 #' named `fs_path` character vector. The names are equivalent to the values,
-#' which is useful for passing onto functions like [purrr::map_dfr()].
+#' which is useful for passing onto functions like `purrr::map_dfr()`.
 #'
 #' `dir_info()` is equivalent to `ls -l` and a shortcut for
 #' `file_info(dir_ls())`.
