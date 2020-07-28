43009759146a68d1740d909f8dabb814abaa6489
Jim Hester <james.f.hester@gmail.com>
2020-07-28 15:18:48 -0400

    Tweak pragmas

--- src/libuv-1.38.1/src/unix/core.c
+++ src/libuv-1.38.1/src/unix/core.c
@@ -521,8 +521,8 @@ int uv__accept(int sockfd) {
  */
 int uv__close_nocancel(int fd) {
 #if defined(__APPLE__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdollar-in-identifier-extension"
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wdollar-in-identifier-extension"
 #if defined(__LP64__) || TARGET_OS_IPHONE
   extern int close$NOCANCEL(int);
   return close$NOCANCEL(fd);
@@ -530,7 +530,7 @@ int uv__close_nocancel(int fd) {
   extern int close$NOCANCEL$UNIX2003(int);
   return close$NOCANCEL$UNIX2003(fd);
 #endif
-#pragma GCC diagnostic pop
+# pragma GCC diagnostic pop
 #elif defined(__linux__)
   return syscall(SYS_close, fd);
 #else
