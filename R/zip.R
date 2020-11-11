b46a4e95abd004dc0da30af49b1f153ad522837d
Kiernan Nicholls <kiernann@protonmail.com>
2020-11-10 22:13:33 -0500

    Use mapply for unzip

--- R/zip.R
+++ R/zip.R
@@ -1,5 +1,6 @@
 #' List zip contents
 #'
+#' @description
 #' Equivalent to the `unzip -l` command. It returns the compressed files within
 #' the given archives, much like [dir_ls()] returns files in a directory. If
 #' more than one archive is provided, files are identified by their source.
@@ -8,16 +9,27 @@
 #' @param method The method to be used. An alternative is to use
 #'   `getOption("unzip")`, which on a Unix-alike may be set to the path to a
 #'   unzip program.
+#' @examples
+#' # List two files from zip
+#' irs <- file_temp(ext = "csv")
+#' write.csv(iris, irs)
+#' mtc <- file_temp(ext = "csv")
+#' write.csv(mtcars, mtc)
+#' zip <- path_temp("two.zip")
+#' zip_create(c(irs, mtc), zip)
+#' zip_ls(zip)
 #' @export
 zip_ls <- function(path, method = "internal") {
   path <- path_real(path)
-  z <- data.frame()
-  for (i in seq_along(path)) {
-    x <- utils::unzip(path[i], list = TRUE, unzip = method)
-    if (length(path) > 1) {
-      x <- cbind(x, source = i)
-    }
-    z <- rbind(z, x)
+  z <- mapply(
+    FUN = utils::unzip, zipfile = path,
+    SIMPLIFY = FALSE, USE.NAMES = TRUE,
+    MoreArgs = list(list = TRUE)
+  )
+  z <- do.call(what = "rbind", args = z)
+  if (length(path) > 1) {
+    z <- cbind(z, source = sub("\\.\\d+$", "", rownames(z)))
+    z$source <- as_fs_path(z$source)
   }
   names(z)[1:3] <- c("path", "size", "date")
   z$path <- as_fs_path(z$path)
@@ -27,6 +39,7 @@ zip_ls <- function(path, method = "internal") {
 
 #' Extract zip contents
 #'
+#' @description
 #' Decompress the contents of a zip archive and extract them to a single
 #' directory so they can be read. Compared to [untils::unzip()], a directory
 #' _must_ be provided in the `dir` directory; files are not automatically
@@ -48,6 +61,13 @@ zip_ls <- function(path, method = "internal") {
 #'   `getOption("unzip")`, which on a Unix-alike may be set to the path to a
 #'   unzip program.
 #' @return The path to the extracted files (invisibly).
+#' @examples
+#' # Extract two files from zip
+#' tmp <- path_temp("iris.csv")
+#' write.csv(iris, tmp)
+#' zip <- zip_create(tmp, junk = TRUE)
+#' out <- zip_move(zip, dir = path_temp("out"))
+#' file_size(c(tmp, zip, out))
 #' @export
 zip_move <- function(path, dir = NULL, files = NULL, overwrite = TRUE,
                      junk = FALSE, method = "internal") {
@@ -56,48 +76,54 @@ zip_move <- function(path, dir = NULL, files = NULL, overwrite = TRUE,
   }
   path <- path_real(path)
   dir <- dir_create(dir)
-  z <- character()
-  for (i in seq_along(path)) {
-    x <- utils::unzip(
-      zipfile = path[i], files = files, overwrite = overwrite,
-      junkpaths = junk, exdir = dir, unzip = method,
+  z <- mapply(
+    FUN = utils::unzip, zipfile = path,
+    SIMPLIFY = TRUE, USE.NAMES = TRUE,
+    MoreArgs = list(
+      files = files, overwrite = overwrite,
+      junkpaths = junk, exdir = dir, unzip = method
     )
-    z <- append(z, x)
-  }
+  )
   invisible(as_fs_path(z))
 }
 
 #' Create zip archive
 #'
+#' @description
 #' A wrapper for an external `zip` command to create zip archives. Unlike
 #' [utils::zip()], filepaths can be passed into the first argument with `%>%`.
 #'
 #' @param path A character vector of files to archive.
 #' @param zip The path of the zip file to create. If none is supplied, and only
-#'   one path is supplied, an archive with that name will be created in the
-#'   same directory as the source file.
+#'   one path is, an archive with that [basename()] will be created in the same
+#'   directory.
 #' @param quiet If `TRUE`, suppress deflating messages (the equivalent of
 #'   `zip -q`).
 #' @param junk If `TRUE`, use only the basename of the path when creating the
 #'   archive (the equivalent of `zip -j`).
+#' @param extra An optional character vector passed to [system()] as arguments
+#'   of the external `zip` command (e.g., `-x` followed by paths to exclude).
 #' @param method A character string specifying the external command to be used.
-#' @return The path to the created archive (invisibly).
+#' @return The path of the created archive (invisibly).
+#' @examples
+#' \dontshow{.old_wd <- setwd(tempdir())}
+#' # Zip a file to same basename
+#' tmp <- path_temp("iris.csv")
+#' write.csv(iris, tmp)
+#' (zip <- zip_create(tmp))
+#' file_size(c(tmp, zip))
+#' \dontshow{setwd(.old_wd)}
 #' @export
 zip_create <- function(path, zip = NULL, quiet = TRUE, junk = FALSE,
-                       method = Sys.getenv("R_ZIPCMD", "zip")) {
+                       extra = "", method = Sys.getenv("R_ZIPCMD", "zip")) {
+  stopifnot(length(zip) == 1)
   path <- path_real(path)
   if (is.null(zip) && length(path) == 1) {
     zip <- path_ext_set(path, "zip")
   } else {
     zip <- path_expand(zip)
   }
-  x <- ""
-  if (junk) {
-    x <- c(x, "-j")
-  }
-  if (quiet) {
-    x <- c(x, "-q")
-  }
-  utils::zip(zipfile = zip, files = path, zip = method, extra = x)
+  x <- c("-j", "-q")[c(junk, quiet)]
+  utils::zip(zipfile = zip, files = path, zip = method, extra = c(exta, x))
   invisible(zip)
 }
