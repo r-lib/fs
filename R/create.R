b46308ce06ae916f1bd317a1c8db37b22c4e1510
David Hall <david.hall.physics@gmail.com>
2021-05-13 12:54:33 -0700

    Handle multiple path arguments

--- R/create.R
+++ R/create.R
@@ -41,7 +41,7 @@ file_create <- function(path, ..., mode = "u=rw,go=r") {
   new <- path_expand(path(path, ...))
 
   .Call(fs_create_, new, as.integer(mode))
-  invisible(path_tidy(path))
+  invisible(path_tidy(new))
 }
 
 #' @export
@@ -59,8 +59,8 @@ dir_create <- function(path, ..., mode = "u=rwx,go=rx", recurse = TRUE, recursiv
   new <- path_expand(path(path, ...))
 
   if (!isTRUE(recurse)) {
-    .Call(fs_mkdir_, path, as.integer(mode))
-    return(invisible(path_tidy(path)))
+    .Call(fs_mkdir_, new, as.integer(mode))
+    return(invisible(path_tidy(new)))
   }
 
   paths <- path_split(new)
@@ -76,7 +76,7 @@ dir_create <- function(path, ..., mode = "u=rwx,go=rx", recurse = TRUE, recursiv
     }
   }
 
-  invisible(path_tidy(path))
+  invisible(path_tidy(new))
 }
 
 #' @export
